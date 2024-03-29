78243c9a18a4e9ab2d7ae74e0e01ba9bbaae3774
unknown <lamkhanh.work@gmail.com>
2024-03-29 08:42:13 +0700

    recipe data model

---
+++ recipes/handler.go
@@ -0,0 +1,22 @@
+package recipes
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/rs/xid"
+)
+
+func NewRecipeHandler(c *gin.Context) {
+	var recipe Recipe
+	if err := c.ShouldBindJSON(&recipe); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error()})
+		return
+	}
+	recipe.ID = xid.New().String()
+	recipe.PublishedAt = time.Now()
+	recipes = append(recipes, recipe)
+	c.JSON(http.StatusOK, recipe)
+}
